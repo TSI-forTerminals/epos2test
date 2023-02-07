0a89b8e01c1536ebfd4a22d3929046f5d9b1eda6
Kazunori Shirokawa <shirokawa@dream.com>
2023-02-07 12:26:54 +0900

    Initial Commit

---
+++ epos2test/epos2testApp.swift
@@ -0,0 +1,17 @@
+//
+//  epos2testApp.swift
+//  epos2test
+//
+//  Created by 城川一理 on 2023/02/07.
+//
+
+import SwiftUI
+
+@main
+struct epos2testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
